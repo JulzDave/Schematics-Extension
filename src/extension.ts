fc5c81328dcae0366f2af697b271680e36bde5a9
Julian Dave Shikui <juliandave1989@gmail.com>
2020-09-21 00:31:41 +0300

    changed app name and externalized data to global consts

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,47 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { relative, resolve, normalize } from "path";
+import { relative, normalize } from "path";
 
+const NX_NESTJS_SCHEMATIC_RELATIVE_LOCATION =
+    "/../schematics/NestJS/src/collection.json";
+const NOTIFY_USER_TO_ADDRESS_TERMINAL =
+    'You have been prompted by the "Mataf Schematics" extension.\nPlease address the integrated terminal to proceed.';
+const enum SchematicFactories {
+    nest = "nest"
+}
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log(
-        'Congratulations, your extension "mataf-nx-nestjs-schematic" is now active!'
+        'Congratulations, your extension "mataf-schematics" is now active!'
     );
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    const absolutePathtoCollection = normalize(__dirname + "/../schematics/NestJS/src/collection.json");
+    const absoluteCollectionPath = normalize(
+        __dirname + NX_NESTJS_SCHEMATIC_RELATIVE_LOCATION
+    );
+    // TODO: look into improving this codeline syntax...
     let remotePath = vscode.workspace.workspaceFolders![0]?.uri.path;
+
+    const clientRelativeCollectionPath = relative(
+        remotePath,
+        absoluteCollectionPath
+    );
 
     if (remotePath[0] === "/" || remotePath[0] === "\\") {
         remotePath = remotePath.substr(1);
     }
 
-    const installSchematicCmd = `schematics "${relative(remotePath, resolve(remotePath, absolutePathtoCollection))+":nest"}" --debug=false --force`;
+    const installSchematicCmd = `schematics "${clientRelativeCollectionPath}:${SchematicFactories.nest}" --debug=false --force`;
 
     // TODO: In package.json, change repository.url to azure repo url
     let disposable = vscode.commands.registerCommand(
-        "mataf-nx-nestjs-schematic.nxNestJSSchematic",
+        "mataf-schematics.nxNestJSSchematic",
         async () => {
             // The code you place here will be executed every time your command is executed
             if (!vscode.window.activeTerminal) {
@@ -41,7 +56,7 @@ export function activate(context: vscode.ExtensionContext) {
                 );
             }
             vscode.window.showInformationMessage(
-                'You have been prompted by "Mataf custom NX-NestJS schematic".\nPlease address the integrated terminal to proceed.'
+                NOTIFY_USER_TO_ADDRESS_TERMINAL
             );
         }
     );
