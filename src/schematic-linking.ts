f3a5e35128026de312a594cc4b4b7026cf980d75
Julian Dave Shikui <juliandave1989@gmail.com>
2020-10-24 22:20:13 +0300

    :ambulance: Added global schematics CLI dependency installation.

--- src/schematic-linking.ts
+++ src/schematic-linking.ts
@@ -1,4 +1,4 @@
-import { window } from 'vscode';
+import { window, Terminal } from 'vscode';
 import { reconsilePath } from './helpers/path-reconsiler';
 import { getExtensionPath } from './helpers/get-extension-path';
 import { F_FIND_FILE } from './helpers/file-searcher';
@@ -13,11 +13,18 @@ const PACKED_SCHEMATIC_PATH = `npm i -g '${reconsilePath(
     extensionPath + '/schematics' + `/${PACKED_SCHEMATIC_FILE_NAME}`,
 )}'`;
 
+const SCHEMATICS_CLI_INSTALLATION = 'npm i -g @angular-devkit/schematics-cli';
+
+function sendTextsToTerminal(terminalInstance: Terminal) {
+    terminalInstance.sendText(SCHEMATICS_CLI_INSTALLATION);
+    terminalInstance.sendText(PACKED_SCHEMATIC_PATH);
+}
+
 export function linkSchematic() {
     if (!ACTIVE_TERMINAL) {
         const newTerminalInstance = window.createTerminal();
-        newTerminalInstance.sendText(PACKED_SCHEMATIC_PATH);
+        sendTextsToTerminal(newTerminalInstance);
     } else {
-        ACTIVE_TERMINAL.sendText(PACKED_SCHEMATIC_PATH);
+        sendTextsToTerminal(ACTIVE_TERMINAL);
     }
 }
